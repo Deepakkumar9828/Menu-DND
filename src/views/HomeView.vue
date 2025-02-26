b60e92f762455b90277303ccc14d6c2ac19b3898
deep.suthar <suthar.deep9079@gmail.com>
2025-02-26 22:08:54 +0530

    clear default files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    Hello this is home view
 </template>
+<script setup lang="ts">
+</script>
